aefa02bf2822c40adea73a03c38efa40ec13259b
Simon Dean <simon@simondean.org>
2013-05-31 03:51:56 +0100

    spawn.js is now the simplest way to reproduce the bug

---
+++ spawn.js
@@ -0,0 +1,31 @@
+var ChildProcess = require('child_process');
+
+child = ChildProcess.spawn('node', ['node_modules/cucumber/bin/cucumber.js', 'features'], {
+  cwd: '.',
+  stdio: 'pipe',
+  env: process.env
+});
+
+var stdout = [];
+
+child.stdout.on('data', function(data) {
+  console.log('stdout');
+  stdout.push(data);
+});
+
+child.stderr.on('data', function(data) {
+  console.log('stderr');
+  console.error(data.toString());
+});
+
+child.on('exit', function(code, signal) {
+  console.log('exit');
+});
+
+child.on('close', function(code, signal) {
+  console.log('close');
+});
+
+child.on('disconnect', function() {
+  console.log('disconnect');
+});
