680d2727ba8555386c8fc4caf630c79bede66173
Simon Dean <simon@simondean.org>
2013-05-31 19:50:32 +0100

    Improved the test

--- spawn_console.js
+++ spawn_console.js
@@ -1,26 +1,48 @@
 var ChildProcess = require('child_process');
 
 var bin = process.argv.length > 2 ? process.argv[2] : 'bin/console';
-console.log('spawning ' + bin);
+console.log('bin: ' + bin);
 
-child = ChildProcess.spawn('node', [bin], {
-  cwd: '.',
-  stdio: ['ignore', 'pipe', process.stderr],
-  env: process.env
-});
+var count = 1000;
+var finishCount = 0;
+var failCount = 0;
 
-child.stdout.on('data', function(data) {
-  console.log('stdout');
-});
+function test() {
+  var child = ChildProcess.spawn('node', [bin], {
+    cwd: '.',
+    stdio: ['ignore', 'pipe', process.stderr],
+    env: process.env
+  });
 
-child.on('exit', function(code, signal) {
-  console.log('exit');
-});
+  var stdoutData = [];
+  
+  child.stdout.on('data', function(data) {
+    stdoutData.push(data);
+  });
 
-child.on('close', function(code, signal) {
-  console.log('close');
-});
+  child.on('exit', function(code, signal) {
+  });
 
-child.on('disconnect', function() {
-  console.log('disconnect');
-});
+  child.on('close', function(code, signal) {
+    finishCount++;
+    
+    if (stdoutData.length == 0) {
+      failCount++;
+    }
+    
+    if (finishCount < count) {
+      test();
+    }
+    else {
+      console.log('Flush succeeded ' + (count - failCount) + ' times');
+      console.log('Flush failed ' + failCount + ' times');
+      console.log('Flush failed ' + (failCount / count * 100) + '% of the time');
+    }
+  });
+
+  child.on('disconnect', function() {
+    console.log('disconnect');
+  });
+}
+
+test();
