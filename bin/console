4e8f1d055557785a091cb458f7f718247c32394a
Simon Dean <simon@simondean.org>
2013-05-31 19:00:46 +0100

    Can reproduce the bug using just built-in node modules

---
+++ bin/console
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log("Hello, world!");
+process.exit(0);
--- spawn_console.js
+++ spawn_console.js
@@ -1,21 +1,13 @@
 var ChildProcess = require('child_process');
 
-child = ChildProcess.spawn('cmd', ['/c', 'node', '--help'], {
+child = ChildProcess.spawn('node', ['bin/console'], {
   cwd: '.',
-  stdio: 'pipe',
+  stdio: ['ignore', 'pipe', process.stderr],
   env: process.env
 });
 
-var stdout = [];
-
 child.stdout.on('data', function(data) {
   console.log('stdout');
-  stdout.push(data);
-});
-
-child.stderr.on('data', function(data) {
-  console.log('stderr');
-  console.error(data.toString());
 });
 
 child.on('exit', function(code, signal) {
